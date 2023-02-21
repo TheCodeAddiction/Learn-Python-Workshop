06098e9372104ccb8497d3d734a95d18362bb1d5
Code addict <jonalgaming@gmail.com>
2023-02-21 13:57:02 +0100

    Final commit without adding bonus tasks since there is not gonna be any time.

--- Whiteboard.py
+++ Whiteboard.py
@@ -1,5 +1,5 @@
-def ta_inn_data_fra_bruker():
-    navn = input("Hva heter du? ")
-    print(navn)
-
-ta_inn_data_fra_bruker()
+navn = "Martin"
+alder = 26
+print("Martin, jeg er 26 år og jeg jobber som utvikler hos PIT")
+print("Anette liker å spise is")
+print("mitt navn er",navn,"jeg er",alder,"år gammel")
